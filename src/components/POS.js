71c80ed6b519a6c5de5b6d77415c7f0058df5571
David Grzyb <grzybdavid@gmail.com>
2019-04-01 17:40:05 -0400

    initial commit

---
+++ src/components/POS.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class POS extends React.Component {
+    state = {
+        search: ''
+    }
+
+    render() {
+        return (
+            <div>POS!</div>
+        );
+    }
+}
